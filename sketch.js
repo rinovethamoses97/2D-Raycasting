c25eca9ace18a3d4217317181313c5f08b25213e
rino <rinovethamoses97@gmail.com>
2019-05-12 11:45:45 +0530

    Boundaries added

--- sketch.js
+++ sketch.js
@@ -6,6 +6,11 @@ function setup(){
     for(var i=0;i<5;i++){
         walls[i]=new Boundary(random(width),random(height),random(width),random(height));
     }
+    // Adding four boundaries
+    walls.push(new Boundary(0,0,width,0));
+    walls.push(new Boundary(0,height,width,height));
+    walls.push(new Boundary(0,0,0,height));
+    walls.push(new Boundary(width,0,width,height));
     particle=new Particle();
 }
 function draw(){
